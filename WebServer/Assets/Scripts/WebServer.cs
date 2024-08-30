88e72fb51b0ccd39f398dfb73c826415bd6174cc
setsuodu <setsuodu@users.noreply.github.com>
2024-08-31 02:20:03 +0800

    检查VPN
    待公网测试

--- WebServer/Assets/Scripts/WebServer.cs
+++ WebServer/Assets/Scripts/WebServer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using UnityEngine;
@@ -39,6 +40,9 @@ public class WebServer
                         try
                         {
                             CheckIP(contenxt);
+                            CheckVPN();
+                            bool vpn = CheckForVPNInterface();
+                            Debug.Log($"vpn : {vpn}");
 
                             string html = _responderMethod(contenxt.Request);
                             byte[] buffer = Encoding.UTF8.GetBytes(html);
@@ -106,5 +110,50 @@ public class WebServer
         Debug.Log($"EnglishName: {englishName}");
         Debug.Log($"DisplayName: {displayName}");
         Debug.Log($"geo: {geo}"); //45→China
+    }
+
+    public bool CheckForVPNInterface()
+    {
+        if (NetworkInterface.GetIsNetworkAvailable())
+        {
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface Interface in interfaces)
+            {
+                // This is the OpenVPN driver for windows. 
+                if (Interface.Description.Contains("TAP-Windows Adapter")
+                  && Interface.OperationalStatus == OperationalStatus.Up)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public bool CheckVPN()
+    {
+        if (NetworkInterface.GetIsNetworkAvailable())
+        {
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface Interface in interfaces)
+            {
+                if (Interface.OperationalStatus == OperationalStatus.Up)
+                {
+                    if ((Interface.NetworkInterfaceType == NetworkInterfaceType.Ppp) && (Interface.NetworkInterfaceType != NetworkInterfaceType.Loopback))
+                    {
+                        IPv4InterfaceStatistics statistics = Interface.GetIPv4Statistics();
+                        Debug.Log(Interface.Name + " " + Interface.NetworkInterfaceType.ToString() + " " + Interface.Description);
+                        return true;
+                    }
+                    else
+                    {
+                        Debug.Log("VPN Connection is lost!");
+                        return false;
+                    }
+                }
+            }
+        }
+        Debug.Log("No Network");
+        return false;
     }
 }
