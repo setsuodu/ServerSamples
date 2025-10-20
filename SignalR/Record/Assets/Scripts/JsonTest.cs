839d0dd0dde9f4c2f5fac75d896764c716d170ee
Xue <setsuodu@users.noreply.github.com>
2025-10-20 15:47:14 +0800

    fix version control file settings

---
+++ SignalR/Record/Assets/Scripts/JsonTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class JsonTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        JsonConvert.SerializeObject(new { test = "test" });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
